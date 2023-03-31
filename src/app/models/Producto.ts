c71f901de9e1b25d5c7e3062658b38da43a2bec0
juansebgo98 <juansebgonzalez10@gmail.com>
2023-03-31 17:19:43 +0200

    TEst usuario commit

--- src/app/models/Producto.ts
+++ src/app/models/Producto.ts
@@ -7,6 +7,13 @@ export class Producto {
   inventarios: Inventario[];
   fechaMasProxima: Date;
 
+  /**
+   * Clase Producto
+   * @param id 
+   * @param nombre 
+   * @param cantidad 
+   * @param inventarios 
+   */
   constructor(id?: number, nombre?: string, cantidad?: number, inventarios?: Inventario[]) {
       this.id = id;
       this.nombre = nombre;
