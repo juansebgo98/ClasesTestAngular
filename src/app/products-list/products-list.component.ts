b57f939368abc0b1bf6f862dd1b2aa4b91163daf
juansebgo98 <juansebgonzalez10@gmail.com>
2023-04-03 23:25:07 +0200

    Implementacion alertas al eliminar y al guardar

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -5,6 +5,7 @@ import { Producto } from '../models/Producto';
 import { InventarioService } from '../models/Services/inventario.service';
 import { ProductoService } from '../models/Services/producto.service';
 import { DatePipe } from '@angular/common';
+import Swal from 'sweetalert2';
 import { FormsModule } from '@angular/forms'; 
 
 @Component({
@@ -53,11 +54,31 @@ export class ProductListComponent implements OnInit {
   }
 
   eliminarProducto(id: number) {
-    if (confirm('¿Estás seguro que deseas eliminar este producto?')) {
-      this.productoService.deleteProducto(id).subscribe(() => {
-        // Remove product from filtered list
-        this.filteredProducts = this.filteredProducts.filter(producto => producto.id !== id);
-      });
-    }
+    Swal.fire({
+      title: '¿Estás seguro que deseas eliminar este producto?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.productoService.deleteProducto(id).subscribe(() => {
+          // Remove product from filtered list
+          this.filteredProducts = this.filteredProducts.filter(producto => producto.id !== id);
+          Swal.fire(
+            '¡Eliminado!',
+            'El producto ha sido eliminado correctamente.',
+            'success'
+          );
+        }, () => {
+          Swal.fire(
+            '¡Error!',
+            'Ha ocurrido un error al eliminar el producto.',
+            'error'
+          );
+        });
+      }
+    });
   }
 }
