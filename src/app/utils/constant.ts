43dc63db2d6d3ff468443985179ebf4a55850436
juansebgo98 <123653322+juansebgo98@users.noreply.github.com>
2023-04-15 00:52:12 +0200

    Update constant.ts

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -2,9 +2,9 @@ import { HttpHeaders } from "@angular/common/http";
 
 export class Constants {
   public static readonly API_URL = 'http://100.79.195.105:8081/api';
-  public static readonly API_URL_PRODUCTO = API_URL+'/productos';
-  public static readonly API_URL_INVENTARIO = API_URL+'/inventarios';
-  public static readonly API_URL_ALMACENAMIENTO = API_URL+'/almacenamiento';
+  public static readonly API_URL_PRODUCTO = Constants.API_URL+'/productos';
+  public static readonly API_URL_INVENTARIO = Constants.API_URL+'/inventarios';
+  public static readonly API_URL_ALMACENAMIENTO = Constants.API_URL+'/almacenamiento';
 /*
   public static readonly API_URL = 'http://localhost:8080/api';
   public static readonly API_URL_PRODUCTO = 'http://localhost:8080/api/productos';
