a840ad3ff23b57ddfb6235e395cccbaae6d38ecf
juansebgo98 <juansebgonzalez10@gmail.com>
2023-05-14 01:53:13 +0200

    Implementado tienda y listado de compra en productos

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -8,16 +8,12 @@ export class Constants {
 
   //public static readonly API_URL = 'http://100.79.195.105/api';
   public static readonly API_URL = 'https://despensaback.duckdns.org/api';
+  //public static readonly API_URL = 'http://localhost:8080/api';
   public static readonly API_URL_PRODUCTO = Constants.API_URL + '/productos';
   public static readonly API_URL_TIENDA = Constants.API_URL + '/tiendas';
   public static readonly API_URL_INVENTARIO = Constants.API_URL + '/inventarios';
   public static readonly API_URL_ALMACENAMIENTO = Constants.API_URL + '/almacenamiento';
-  /*
-    public static readonly API_URL = 'http://localhost:8080/api';
-    public static readonly API_URL_PRODUCTO = 'http://localhost:8080/api/productos';
-    public static readonly API_URL_INVENTARIO = 'http://localhost:8080/api/inventarios';
-    public static readonly API_URL_ALMACENAMIENTO = 'http://localhost:8080/api/almacenamiento';
-  */
+
   public static readonly httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
 }
 
